6b81b23a46e5c6a16abb75ebd6c3baf5e938b3ad
LeeJungHo <dldi121@naver.com>
2022-02-28 16:43:42 +0900

    'end_1'

--- VIEW/JS/scripts.js
+++ VIEW/JS/scripts.js
@@ -75,10 +75,52 @@ window.addEventListener('DOMContentLoaded', event => {
     // Activate SimpleLightbox plugin for portfolio items
     var img_path = [];
     
-    var java_pos = new SimpleLightbox('#portfolio img.javas', {elements: '#portfolio a.javas'})
-    /*new SimpleLightbox({
+    new SimpleLightbox({
         elements: '#portfolio a.javas'
-    });*/
+    });
+    
+  
+    $(window).on("load", function () {
+    function fade() {
+      let animation_height = $(window).innerHeight() * 0.5;
+      let ratio = Math.round((1 / animation_height) * 10000) / 10000;
+      $(".fade").each(function () {
+        let objectTop = $(this).offset().top;
+        let windowBottom = $(window).scrollTop() + $(window).innerHeight();
+        if (objectTop < windowBottom) {
+          if (objectTop < windowBottom - animation_height) {
+            $(this).css({
+              transition: "opacity 0.1s linear",
+              transition: "left 0.1s linear",
+              opacity: 1,
+              left: "0px",
+            });
+          } else {
+            $(this).css({
+              transition: "opacity 0.5s linear",
+              opacity: (windowBottom - objectTop) * ratio,
+              transition: "left 0.5s linear",
+              left: `${200 * (1 - (windowBottom - objectTop) * ratio)}px`,
+            });
+          }
+        } else {
+          $(this).css({
+            opacity: 0,
+            left: "200px",
+          });
+        }
+      });
+    }
+    $(".fade").css({
+      opacity: 0,
+      left: "200px",
+    });
+    fade();
+
+    $(window).scroll(function () {
+      fade();
+    });
+  });
     
 });
 
